fbbafdcaf1a700693022b3a99c0d084728bb4fe2
Fabian Betancourt <fabianrbv@gmail.com>
2025-11-05 00:01:53 -0500

    W01 Project: Contacts Part 1

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const contactsRouter = require('./routes/contacts');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/contacts', contactsRouter);
+
+// Port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
