be7d859a946653b85998d4e8e52ee9b21e3c7e4f
Fabian Betancourt <fabianrbv@gmail.com>
2025-11-11 20:57:38 -0500

    prueba para que render funcione

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 // Routes
 app.use('/contacts', contactsRouter);
