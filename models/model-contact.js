8c901df8f8a7de052c66c39c05dfa7727741adc3
Fabian Betancourt <fabianrbv@gmail.com>
2025-11-22 09:16:02 -0500

    Final commit 2.0

--- models/model-contact.js
+++ models/model-contact.js
@@ -1,38 +1,5 @@
 const mongoose = require('mongoose');
 
-/**
- * @openapi
- * components:
- *   schemas:
- *     Contact:
- *       type: object
- *       required:
- *         - firstName
- *         - lastName
- *         - email
- *         - favoriteColor
- *         - birthday
- *       properties:
- *         _id:
- *           type: string
- *           example: "507f1f77bcf86cd799439011"
- *         firstName:
- *           type: string
- *           example: "John"
- *         lastName:
- *           type: string
- *           example: "Doe"
- *         email:
- *           type: string
- *           example: "john@example.com"
- *         favoriteColor:
- *           type: string
- *           example: "blue"
- *         birthday:
- *           type: string
- *           example: "1990-01-15"
- */
-
 const contactSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
